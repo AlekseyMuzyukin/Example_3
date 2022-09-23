bc57b31a9b1754a8708f4db47a6cd7cdfa98dbac
Aleksey <J2k_pot@mail.ru>
2022-09-23 10:23:03 +0300

    add task 23

---
+++ task_23/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+void TableCub(int arg)
+{
+    int i = 1;
+    if(arg > 0)
+    {
+        while(i <= arg)
+        {
+            Console.WriteLine($"{i,3} {Math. Pow(i, 3),3}");
+            i++;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели отрицательное число"); 
+    }
+}
+
+TableCub(num);
