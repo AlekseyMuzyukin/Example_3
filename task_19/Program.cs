1f0005f1ac1ff70e88843e88ba3836826ab4d6d3
Aleksey <J2k_pot@mail.ru>
2022-09-23 10:18:34 +0300

    add task 19

---
+++ task_19/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+void Palindrom(int arg)
+{
+    if (arg < 99999 && arg > 9999)
+    {
+        int first = arg % 10;
+        int recFirst = (arg / 10000) % 10;
+        int recSecond = (arg / 1000) % 10;
+        int sekond = (arg / 10) % 10;
+
+        if (first == recFirst && recSecond == sekond)
+        {
+            Console.WriteLine("Число является полиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Число не является полиндромом");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ввели неверное число");
+    }
+}
+Palindrom(num);
